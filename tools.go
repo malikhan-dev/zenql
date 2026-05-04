4fc44eb03e49396b404e8392fbad48451ecd0e11
mohammadreza malikhan <109470179+malikhan-dev@users.noreply.github.com>
2026-05-04 21:34:21 +0330

    Create tools.go

---
+++ tools.go
@@ -0,0 +1,31 @@
+package lingo
+
+func DeepCopy[T any](items []T) *[]T {
+
+	destination := make([]T, len(items))
+
+	destIndex := 0
+
+	for _, v := range items {
+		destination[destIndex] = v
+		destIndex++
+	}
+
+	return &destination
+}
+
+func ToSlice[k int | float64 | float32 | int16 | int32 | uint | int64, v any](item map[k]v) *[]v {
+
+	var destinationSlice []v
+
+	destIndex := 0
+
+	destinationSlice = make([]v, len(item))
+
+	for _, val := range item {
+		destinationSlice[destIndex] = val
+		destIndex++
+	}
+	return &destinationSlice
+
+}
