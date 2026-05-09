ed7afe65bc07602f2b54a6afeb3e79eaa188b545
mohammadreza malikhan <109470179+malikhan-dev@users.noreply.github.com>
2026-05-09 04:54:59 +0330

    Update collectors.go
    had to upload the whole file. bad net

--- collectors.go
+++ collectors.go
@@ -19,6 +19,25 @@ func (query *Queryable[T]) CollectRange(cnt int) ([]T, []OpError) {
 
 }
 
+func (query *Queryable[T]) CollectChan(BufferSize int) <-chan CollectStream[T] {
+
+	ch := make(chan CollectStream[T], BufferSize)
+
+	go func() {
+
+		defer close(ch)
+
+		for _, v := range query.Items {
+			ch <- CollectStream[T]{Value: v}
+		}
+		for _, v := range query.Err {
+			ch <- CollectStream[T]{Err: v}
+		}
+
+	}()
+	return ch
+}
+
 func (query *GroupedQueryable[K, T]) Collect() (map[K][]T, []OpError) {
 	return query.Items, query.Err
 }
