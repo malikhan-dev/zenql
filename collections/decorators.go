aa6063f71f3044a70df0a226b5e65b5567948b15
mohammadreza malikhan <109470179+malikhan-dev@users.noreply.github.com>
2026-05-13 22:34:54 +0330

    Update decorators.go

--- collections/decorators.go
+++ collections/decorators.go
@@ -25,11 +25,7 @@ func (query *Queryable[T]) Where(fieldName string, fieldValue any) *Queryable[T]
 }
 
 func (query *Queryable[T]) All() *Queryable[T] {
-	if len(query.Items) > 0 {
-		return query
-	} else {
-		panic(ErrFactory(4, ""))
-	}
+	return AAll(query)
 }
 
 func (query *Queryable[T]) First() *Queryable[T] {
@@ -46,75 +42,19 @@ func (query *Queryable[T]) FirstOrDefault() *Queryable[T] {
 
 func GroupBy[K comparable, T any](query *Queryable[T], fieldName string) *GroupedQueryable[K, T] {
 
-	var result GroupedQueryable[K, T]
-
-	mapped := make(map[K][]T)
-
-	strType := reflect.TypeFor[T]()
-
-	for _, val := range query.Err {
-		result.Err = append(result.Err, val)
-	}
-
-	if strType.Kind() == reflect.Ptr {
-		strType = strType.Elem()
-	}
-
-	if strType.Kind() != reflect.Struct {
-		result.Err = append(result.Err, ErrFactory(3, strType.Name()))
-		return &result
-	}
-
-	targetField, ok := strType.FieldByName(fieldName)
-	if !ok {
-		result.Err = append(result.Err, ErrFactory(2, fieldName))
-		return &result
-	}
-
-	for _, val := range query.Items {
-
-		RowVal := reflect.ValueOf(val)
-		if RowVal.Kind() == reflect.Ptr {
-			RowVal = RowVal.Elem()
-		}
-
-		RowField := RowVal.FieldByIndex(targetField.Index)
-
-		key := RowField.Interface()
-
-		if !reflect.TypeOf(key).Comparable() {
-			result.Err = append(result.Err, ErrFactory(6, fieldName))
-			break
-		}
-
-		k, ok := key.(K)
-
-		if !ok {
-
-			result.Err = append(result.Err, ErrFactory(6, fieldName))
-			break
-
-		} else {
-			mapped[k] = append(mapped[k], val)
-		}
-
-	}
-
-	result.Items = mapped
-	return &result
-
+	return GGroupBy[K, T](query, fieldName)
 }
 
 func (query *GroupedQueryable[K, T]) CollectGroup() (map[K][]T, []contracts.OpError) {
 	return CCollectGroup[K, T](query)
 }
 
-func (query *Queryable[T]) CCount() int {
-	return len(query.Items)
+func (query *Queryable[T]) Count() int {
+	return CCount(query)
 }
 
-func (query *Queryable[T]) EErrCount() int {
-	return len(query.Err)
+func (query *Queryable[T]) ErrCount() int {
+	return EErrCount(query)
 }
 
 func (query *Queryable[T]) Collect() ([]T, []contracts.OpError) {
