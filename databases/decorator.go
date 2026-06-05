2e188ef9cb992b43142ae6c72b4a857862f060d0
mohammadreza malikhan <109470179+malikhan-dev@users.noreply.github.com>
2026-06-05 18:01:48 +0330

    Create decorator.go

---
+++ databases/decorator.go
@@ -0,0 +1,25 @@
+package databases
+
+/*
+ * Author: Mohammadreza Malikhan
+ * License: MIT
+ */
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/malikhan-dev/zenq/contracts"
+	"github.com/malikhan-dev/zenq/streams"
+)
+
+func FromSqlRows[T any](ctx context.Context, conn contracts.RDBMSFacade, query string, Mapper func(rows *sql.Rows) (T, error), args ...any) streams.Streamable[T] {
+	stream, err := frmSqlRows[T](ctx, conn, query, Mapper, args...)
+	return streams.Streamable[T]{
+		Context:    ctx,
+		Channel:    stream,
+		BufferSize: 256,
+		Err:        []error{err},
+		Initiated:  err == nil,
+	}
+}
