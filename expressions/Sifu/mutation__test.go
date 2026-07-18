a80d7803754c7f25eb0088454924c1c327866c89
mohammadreza malikhan <malikhan.dev2020@gmail.com>
2026-07-18 19:27:57 +0330

    added a test case for strIn() (beside integration tests)
    added a test case for SetStruct() (beside integration tests)
    
    added a test case for AppStruct() (beside integration tests)
    
    Improving Sifu mutators Performance

--- expressions/Sifu/mutation__test.go
+++ expressions/Sifu/mutation__test.go
@@ -24,47 +24,6 @@ func TestAppStr(t *testing.T) {
 
 }
 
-func TestAppStrUnexported(t *testing.T) {
-
-	type args struct {
-		name string
-	}
-
-	var list []args
-
-	list = append(list, args{name: "a"})
-
-	expr := Expr[args]().Prop("name").StrApp(" bcD").Predicate()
-
-	list[0] = expr(list[0])
-
-	if list[0].name != "a" {
-		t.Errorf("expected %q, got %q", "a bcD", list[0].name)
-	} // name is unexported, no changes should happen
-
-}
-
-func TestSetBoolUnexported(t *testing.T) {
-
-	type args struct {
-		name     string
-		pressent bool
-	}
-
-	var list []args
-
-	list = append(list, args{name: "a", pressent: false})
-
-	expr := Expr[args]().Prop("pressent").SetBool(true).Predicate()
-
-	list[0] = expr(list[0])
-
-	if list[0].pressent {
-		t.Errorf("expected false, got true")
-	} // pressent is unexported, no changes should happen
-
-}
-
 func TestSetBool(t *testing.T) {
 
 	type args struct {
